52318837a095c9edc036301fa5702f60e1bd178f
Karsten Richter <kr@corika.com>
2017-03-02 12:09:09 +0100

    add filenames and different invoice types

--- app/controllers/corika_invoices/application_controller.rb
+++ app/controllers/corika_invoices/application_controller.rb
@@ -1,4 +1,7 @@
 module CorikaInvoices
   class ApplicationController < ActionController::Base
+    def current_user
+      current_admin
+    end
   end
 end
--- app/helpers/corika_invoices/application_helper.rb
+++ app/helpers/corika_invoices/application_helper.rb
@@ -1,4 +1,57 @@
 module CorikaInvoices
   module ApplicationHelper
+    def link_to_show(obj,params=nil)
+      
+      txtmode= params[:txtmode] unless params.nil?
+      txt = params[:txt] unless params.nil?
+
+      namespace=nil
+      entity = nil
+
+      if obj.kind_of?(Array) then
+        namespace=  obj[0]
+        entity = obj[1]
+      else 
+        entity = obj
+      end
+
+
+      if txt.nil? then
+        txt = t('common.show')
+      end
+
+      if can? :read, entity
+
+        if txtmode then
+          link_to txt, obj
+        else 
+            link_to txt, obj, :class => "btn btn-default btn-xs" 
+        end
+      end
+    end
+
+    def entity_row(entity, field,type=nil)
+      sym = entity.class.name.underscore.to_sym
+      content_tag :div, :class => "row" do
+        concat(content_tag(:div, label(sym, field), :class => "col-md-3 text-right"))
+
+        tmp_data = entity[field]
+
+        if tmp_data.nil? then
+          return ""
+        end
+        data = nil
+        if type.nil?
+          data = tmp_data
+        elsif type == :date
+          data = l tmp_data
+        elsif type == :mailto
+          data = mail_to tmp_data,tmp_data
+        elsif type == :currency
+          data = format_currency tmp_data,"EUR"
+        end
+        concat(content_tag(:div, data,:class => "col-md-9"))
+      end
+    end
   end
 end

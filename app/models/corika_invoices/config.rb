c965acfb6afca2f01d27ecbdb86fb4d59066309d
Karsten Richter <kr@corika.com>
2017-10-29 08:32:25 +0100

    add tex branding dir to config

--- app/models/corika_invoices/config.rb
+++ app/models/corika_invoices/config.rb
@@ -1,6 +1,6 @@
 module CorikaInvoices
   class Config 
-    attr_accessor  :work_dir, :tool_dir, :archive_dir,:creditor_id,:company,:company_short,:iban,:bic,:bank, :tex_exe, :tex_dir
+    attr_accessor  :work_dir, :tool_dir, :archive_dir,:creditor_id,:company,:company_short,:iban,:bic,:bank, :tex_exe, :tex_dir, :tex_branding_dir
 
     def initialize(hash) 
 

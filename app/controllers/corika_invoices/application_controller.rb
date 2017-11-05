adfc27a23eadb7fbeb3fec85c92c506b476d1d52
Karsten Richter <kr@corika.com>
2017-11-05 22:04:00 +0100

    add main app routes helper

--- app/controllers/corika_invoices/application_controller.rb
+++ app/controllers/corika_invoices/application_controller.rb
@@ -1,5 +1,6 @@
 module CorikaInvoices
   class ApplicationController < ::ApplicationController 
+    helper Rails.application.routes.url_helpers
 #    def current_user
 #      current_admin
 #    end

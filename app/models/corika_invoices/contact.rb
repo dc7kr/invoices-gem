f0e2884ed855ce12d23808a49c1a8be06d46fe9b
Karsten Richter <kr@corika.com>
2025-11-06 17:29:19 +0000

    make contact a mongo db document
    - implement to_hash for yaml serialization

--- app/models/corika_invoices/contact.rb
+++ app/models/corika_invoices/contact.rb
@@ -1,16 +1,48 @@
+require 'yaml'
+
 module CorikaInvoices
   class Contact
-    attr_accessor :iban, :bic, :phone, :fax, :mail, :name, :dept, :street, :city, :zip, :job, :bank
+    include Mongoid::Document
 
-    # for yaml generation
-    include Hashify
+    field :iban, type: String
+    field :bic, type: String
+    field :phone, type: String
+    field :fax, type: String
+    field :email, type: String
+    field :name, type: String
+    field :dept, type: String
+    field :street, type: String
+    field :city, type: String
+    field :zip, type: String
+    field :job, type: String
+    field :bank, type: String
+
+    field :company, type: String
+    field :company_short, type: String
+    field :vat_id, type: String
+    field :tax_reg, type: String
+    field :creditor_id, type: String
+    field :country_id, type: String
+
 
     def initialize(hash)
       throw :contact_data_nil if hash.nil?
+      super
 
       hash.each do |k, v|
         public_send("#{k}=", v)
       end
+
+      self.company = INVOICE_CONFIG.payee.company
+      self.company_short = INVOICE_CONFIG.payee.company_short
+      self.vat_id = INVOICE_CONFIG.payee.vat_id
+      self.tax_reg = INVOICE_CONFIG.payee.tax_reg
+      self.creditor_id = INVOICE_CONFIG.payee.creditor_id
+      
+      # only use default IBAN/BIC/Bank if it is not overridden by contact hash
+      self.iban = INVOICE_CONFIG.payee.creditor_id if self.iban.nil?
+      self.bank = INVOICE_CONFIG.payee.bank if self.bank.nil?
+      self.bic = INVOICE_CONFIG.payee.bic if self.bic.nil?
     end
 
     def valid?
@@ -19,6 +51,29 @@ module CorikaInvoices
 
     def bank_account?
       !iban.nil? and !bic.nil?
+    end
+
+    def to_hash
+      {
+        :iban => iban,
+        :bic => bic,
+        :phone => phone,
+        :fax =>  fax,
+        :email => email,
+        :name => name,
+        :dept => dept,
+        :street => street,
+        :city => city,
+        :country_id=> country_id,
+        :zip => zip,
+        :job => job,
+        :bank => bank,
+        :company => company,
+        :company_short => company_short,
+        :vat_id => vat_id,
+        :tax_reg => tax_reg,
+        :creditor_id => creditor_id
+      }
     end
   end
 end

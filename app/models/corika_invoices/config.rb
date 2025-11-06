0e7cb4af95e7f1adf6211f6f0aa6fd724f5467ac
Karsten Richter <kr@corika.com>
2025-11-06 17:28:51 +0000

    add payee contact info to config

--- app/models/corika_invoices/config.rb
+++ app/models/corika_invoices/config.rb
@@ -1,13 +1,17 @@
 module CorikaInvoices
   class Config
-    attr_accessor :work_dir, :tool_dir, :archive_dir, :creditor_id, :company, :company_short, :iban, :bic, :bank, :tex_exe,
-                  :tex_dir, :tex_branding_dir, :message_prefix, :taxrate, :taxrate_reduced, :header_file
+    attr_accessor :work_dir, :tool_dir, :archive_dir, 
+                  :tex_exe, :payee, :tex_dir, :tex_branding_dir, :message_prefix, :taxrate, :taxrate_reduced, :header_file
 
     def initialize(hash)
       throw :invoice_config_data_nil if hash.nil?
 
       hash.each do |k, v|
-        public_send("#{k}=", v) if respond_to? "#{k}="
+        if k == "payee" 
+          self.payee = CorikaInvoices::PayeeContact.new(v)
+        else 
+          public_send("#{k}=", v) if respond_to? "#{k}="
+        end
       end
     end
   end

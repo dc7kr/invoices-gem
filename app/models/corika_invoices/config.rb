96c96fa67f3e635e21f86e0128f7f4bc4e339f0b
Karsten Richter <kr@corika.com>
2025-11-17 15:25:55 +0000

    adjust config parameters for new yaml based LaTeX/PDF generator.

--- app/models/corika_invoices/config.rb
+++ app/models/corika_invoices/config.rb
@@ -1,16 +1,17 @@
 module CorikaInvoices
   class Config
-    attr_accessor :work_dir, :tool_dir, :archive_dir, 
-                  :tex_exe, :payee, :tex_dir, :tex_branding_dir, :message_prefix, :taxrate, :taxrate_reduced, :header_file
+    attr_accessor :input_dir, :output_dir, :tool_dir, :archive_dir, :template_dir,
+                  :payee, :message_prefix, :taxrate, :taxrate_reduced,
+                  :tex_bin, :tex_dir, :fonts_dir, :custom_dir, :default_tax_mode
 
     def initialize(hash)
       throw :invoice_config_data_nil if hash.nil?
 
       hash.each do |k, v|
-        if k == "payee" 
+        if k == 'payee'
           self.payee = CorikaInvoices::PayeeContact.new(v)
-        else 
-          public_send("#{k}=", v) if respond_to? "#{k}="
+        elsif respond_to? "#{k}="
+          public_send("#{k}=", v)
         end
       end
     end

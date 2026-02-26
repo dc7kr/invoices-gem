dd2b27b2f8c7e1d4a626675b2b283628b54f9c90
Karsten Richter <kr@corika.com>
2026-02-26 17:11:00 +0000

    remove apps url helper

--- app/controllers/corika_invoices/application_controller.rb
+++ app/controllers/corika_invoices/application_controller.rb
@@ -1,8 +1,4 @@
 module CorikaInvoices
   class ApplicationController < ::ApplicationController
-    helper Rails.application.routes.url_helpers
-    #    def current_user
-    #      current_admin
-    #    end
   end
 end
